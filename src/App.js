6a66fb8d542d0252a173c27372c52e562bfec8e2
Levis Panseriya <levispanseriya.78@gmail.com>
2023-07-08 18:39:17 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useSelector, useDispatch } from "react-redux";
+import { incNumber, decNumber } from "./actions/index";
 
 function App() {
+  const myState = useSelector((state) => state.changeTheNumber);
+  const disPatch = useDispatch();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <h1>Increnment/Decrenment</h1>
+      <h4>using React/Redux</h4>
+
+      <div className="quentity">
+        <button
+          className="q_minus"
+          title="Decrenment"
+          onClick={() => disPatch(decNumber)}
         >
-          Learn React
-        </a>
-      </header>
+          <span>-</span>
+        </button>
+        <input
+          name="quentity"
+          type="text"
+          class="quentity_input"
+          value={myState}
+        />
+        <button
+          className="q_plus"
+          title="Increnment"
+          onClick={() => disPatch(incNumber)}
+        >
+          <span>+</span>
+        </button>
+      </div>
     </div>
   );
 }
